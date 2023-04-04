efa32b62279d7850d9f02ee6dcef56abc32a056e
Jair Borges Ferraz <jbferraz@senacinformatica.br>
2023-04-04 11:56:56 -0300

    ClienteDAO: cadastrar, listar e getClienteByCPF pronto

--- src/dao/ClienteDAO.java
+++ src/dao/ClienteDAO.java
@@ -7,8 +7,10 @@ package dao;
 
 import conexao.Conexao;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import model.Cliente;
@@ -36,5 +38,48 @@ public class ClienteDAO {
             System.out.println("Erro ao cadastrar!\n"
                     + ex.getMessage());
         }
+    }//fim cadastrar
+
+    public ArrayList<Cliente> getClienteBD() {
+        Connection con = Conexao.getConexao();
+        try {
+            Statement stat = con.createStatement();
+            String sql = "select * from clientes";
+            ResultSet rs = stat.executeQuery(sql);
+            ArrayList<Cliente> clientes = new ArrayList<>();
+            while (rs.next()) {
+                Cliente c = new Cliente();
+                //lado do java |x| (lado do banco)
+                c.setIdCliente(rs.getInt("idcliente"));
+                c.setNomeCliente(rs.getString("nome"));
+                c.setCpf(rs.getString("cpf"));
+                clientes.add(c);
+            }
+            return clientes;
+        } catch (SQLException ex) {
+            System.out.println("Erro ao Listar!\n"
+                    + ex.getMessage());
+        }
+        return null;
+    }//fim do listar
+
+    public Cliente getClienteByDoc(String cpf) {
+        Connection con = Conexao.getConexao();
+        Cliente c = null;
+        try {
+            Statement stat = con.createStatement();
+            String sql = "select * from clientes where cpf = '" + cpf +"'";
+            ResultSet rs = stat.executeQuery(sql);
+            while (rs.next()) {
+                //lado do java |x| (lado do banco)
+                c.setIdCliente(rs.getInt("idcliente"));
+                c.setNomeCliente(rs.getString("nome"));
+                c.setCpf(rs.getString("cpf"));
+            }
+        } catch (SQLException ex) {
+            System.out.println("Erro ao consultar CPF!\n"
+                    + ex.getMessage());
+        }
+        return c;
     }
 }
