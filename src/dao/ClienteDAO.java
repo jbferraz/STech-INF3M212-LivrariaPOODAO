6f0307f9f067d0fd6c3a2fce3d7118b4a25b0a99
Jair Borges Ferraz <jbferraz@senacinformatica.br>
2023-04-05 11:58:46 -0300

    DAOFactory do Cliente e ClienteServicos Pronto

--- src/dao/ClienteDAO.java
+++ src/dao/ClienteDAO.java
@@ -7,12 +7,11 @@ package dao;
 
 import conexao.Conexao;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import model.Cliente;
 
 /**
@@ -27,20 +26,21 @@ public class ClienteDAO {
         try {
             //cria espaço de trabalho SQl, é a área no Java onde
             //vamo executar os scripts SQL
-            Statement stat = con.createStatement();
             String sql;
-            sql = "insert into clientes values "
-                    + "(null, "
-                    + "'" + cVO.getNomeCliente() + "',"
-                    + "'" + cVO.getCpf() + "');";
-            stat.execute(sql);
+            sql = "insert into clientes values (null, ?,?,null,?,?)";
+            PreparedStatement pst = con.prepareStatement(sql);
+            pst.setString(1, cVO.getNomeCliente());
+            pst.setString(2, cVO.getCpf());
+            pst.setString(3, cVO.getEndereco());
+            pst.setString(4, cVO.getTelefone());
+            pst.executeUpdate();
         } catch (SQLException ex) {
             System.out.println("Erro ao cadastrar!\n"
                     + ex.getMessage());
         }
     }//fim cadastrar
 
-    public ArrayList<Cliente> getClienteBD() {
+    public ArrayList<Cliente> getClientesDAO() {
         Connection con = Conexao.getConexao();
         try {
             Statement stat = con.createStatement();
@@ -53,6 +53,8 @@ public class ClienteDAO {
                 c.setIdCliente(rs.getInt("idcliente"));
                 c.setNomeCliente(rs.getString("nome"));
                 c.setCpf(rs.getString("cpf"));
+                c.setEndereco(rs.getString("endereco"));
+                c.setTelefone(rs.getString("telefone"));
                 clientes.add(c);
             }
             return clientes;
@@ -67,19 +69,52 @@ public class ClienteDAO {
         Connection con = Conexao.getConexao();
         Cliente c = null;
         try {
-            Statement stat = con.createStatement();
-            String sql = "select * from clientes where cpf = '" + cpf +"'";
-            ResultSet rs = stat.executeQuery(sql);
+            String sql = "select * from clientes where cpf = ?";
+            PreparedStatement pst = con.prepareStatement(sql);
+            pst.setString(1, cpf);
+            ResultSet rs = pst.executeQuery(sql);
             while (rs.next()) {
                 //lado do java |x| (lado do banco)
                 c.setIdCliente(rs.getInt("idcliente"));
                 c.setNomeCliente(rs.getString("nome"));
                 c.setCpf(rs.getString("cpf"));
+                c.setEndereco(rs.getString("endereco"));
+                c.setTelefone(rs.getString("telefone"));
             }
         } catch (SQLException ex) {
             System.out.println("Erro ao consultar CPF!\n"
                     + ex.getMessage());
         }
         return c;
+    }
+    
+    public void deletarClienteDAO(String cpf){
+        Connection con = Conexao.getConexao();
+        try {
+            String sql = "delete from clientes where cpf = ?";
+            PreparedStatement pst = con.prepareStatement(sql);
+            pst.setString(1, cpf);
+            pst.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println("Erro ao deletar CPF!\n"
+                    + ex.getMessage());
+        }
+    }//fim deletarCliente
+    
+    public void atualizaClienteByDoc(Cliente cVO){
+        Connection con = Conexao.getConexao();
+        try {
+            String sql = "update clientes set nome = ?, endereco = ?, telefone = ? "
+                    + "where cpf = ?";
+            PreparedStatement pst = con.prepareStatement(sql);
+            pst.setString(1, cVO.getNomeCliente());
+            pst.setString(2, cVO.getEndereco());
+            pst.setString(3, cVO.getTelefone());
+            pst.setString(4, cVO.getCpf());
+            pst.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println("Erro ao atualizar CPF!\n"
+                    + ex.getMessage());
+        }
     }
 }
---
+++ src/services/ClienteServicos.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services;
+
+import dao.ClienteDAO;
+import dao.DAOFactory;
+import java.util.ArrayList;
+import model.Cliente;
+
+/**
+ *
+ * @author jbferraz
+ */
+public class ClienteServicos {
+
+    public void cadCliente(Cliente cVO) {
+        ClienteDAO cDAO = DAOFactory.getClienteDAO();
+        cDAO.cadastrarClienteDAO(cVO);
+    }
+    
+    public void atualizarCliente(Cliente cVO){
+        ClienteDAO cDAO = DAOFactory.getClienteDAO();
+        cDAO.atualizaClienteByDoc(cVO);
+    }
+    
+    public void deletarCliente(String cpf){
+        ClienteDAO cDAO = DAOFactory.getClienteDAO();
+        cDAO.deletarClienteDAO(cpf);
+    }
+    
+    public Cliente buscarClientebyCPF(String cpf){
+        ClienteDAO cDAO = DAOFactory.getClienteDAO();
+        return cDAO.getClienteByDoc(cpf);
+    }
+    
+    public ArrayList<Cliente> getClientes(){
+        ClienteDAO cDAO = DAOFactory.getClienteDAO();
+        return cDAO.getClientesDAO();
+    }
+}
